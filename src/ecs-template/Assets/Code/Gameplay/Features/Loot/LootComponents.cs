75cd220612ce3545fd6b78d5f14d330eb68b5ea5
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-03-08 20:05:40 +0200

    Edits folders

--- src/ecs-template/Assets/Code/Gameplay/Features/Loot/LootComponents.cs
+++ src/ecs-template/Assets/Code/Gameplay/Features/Loot/LootComponents.cs
@@ -7,7 +7,9 @@ namespace Code.Gameplay.Features.Loot
     [Game] public class Pulling : IComponent { }
     [Game] public class Collected : IComponent { }
     
-    [Game] public class PickupRadius : IComponent { public float Value;}
+    [Game] public class PickupRadius : IComponent { public float Value; }
+    
+    [Game] public class PickupRadiusS : IComponent { public float Value; }
     
     [Game] public class Experience : IComponent { public float Value; }
 }
